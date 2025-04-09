f89882b62dc305e98d0e9062b575097d638d029f
Matheus Coronado <matheuscoronado48@gmail.com>
2025-04-08 21:45:10 -0300

    Criado função para pegar e armazenar o campo de dia e o campo de cor. Criado função para testar se o campo dia está preenchido

---
+++ main.js
@@ -0,0 +1,21 @@
+function colorirDia(){
+    //Pegar valor de campo de dia
+    let days = document.querySelector('days').value;
+
+    //Pega o valor do campo da cor
+    let color = document.getElementById('calendar');
+
+    //Verifica se o campo de dia foi preenchido
+    if(!days){
+        alert('Favor informar o dia');
+    }
+    else if((days > 0) && (days < 31)){
+        // Se o número de dias está no intervalo | Aplicará a cor na célula do calendário
+        let td = calendar.getElementByTagName('td');
+        td.style.backgroundColor = color;
+    }
+    else{
+        alert('Favor informar um dia do calendário')
+    }
+
+}
